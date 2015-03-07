b36667f241f81035d8795ccfdd7e992c50ca1701
Erik Klop <e.klop@iblux.nl>
2015-03-07 20:42:40 +0100

    Added a flickering effect for touch devices

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,4 +13,30 @@ $(document).ready(function() {
 	$('#grapes').mouseover(run);
 	// end the fox and the grapes scripts
 
+	// the lamp scripts
+	function litlamp()
+	{
+		$('.section-the-lamp').toggleClass('lit');
+	}
+
+	function randRange(data)
+	{
+       var newTime = data[Math.floor(data.length * Math.random())];
+       return newTime;
+	}
+
+	function toggleSomething()
+	{
+		//var timeArray = new Array(200, 400, 600, 1200, 1400, 1600, 4000);
+		var timeArray = new Array(50, 80, 50, 1000, 600, 150, 1200);
+		
+		litlamp();
+		clearInterval(timer);
+		timer = setInterval(toggleSomething, randRange(timeArray));
+	}
+
+	var timer = setInterval(toggleSomething, 1000);
+
+	//end of the lamp scripts
+
 });
